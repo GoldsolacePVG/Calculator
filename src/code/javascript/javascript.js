8db8480581813d3f502982d1a650392b7f4c61d3
GoldsolacePVG <rodrigomicopliego@gmail.com>
2023-10-11 10:11:00 +0200

    added funcs

--- src/code/javascript/javascript.js
+++ src/code/javascript/javascript.js
@@ -1,3 +1,8 @@
+/**
+ * @brief Add characters to text box.
+ * 
+ * @param {*} value Chracater that will be added to the text box.
+ */
 function AddToDisplay(value) {
   var aux_value = document.getElementById('display').value;
 
@@ -11,10 +16,16 @@ function AddToDisplay(value) {
   document.getElementById('display').value = aux_value;
 }
 
+/**
+ * @brief Clean text box.
+ */
 function CleanDisplay(){
   document.getElementById('display').value = 0;
 }
 
+/**
+ * @brief Function to calculate de radic.
+ */
 function RadicCalculation(){
   var aux_value = document.getElementById('display').value;
   
@@ -23,10 +34,35 @@ function RadicCalculation(){
   document.getElementById('display').value = aux_value;
 }
 
+/**
+ * @brief Evaluates the text box and calculates depending on what appear on it.
+ */
 function Calculation(){
   try{
     document.getElementById('display').value = eval(document.getElementById('display').value);
   }catch(error){
     document.getElementById('display').value = 'Error';
   }
+}
+
+/**
+ * @brief Function to multiply per the same number.
+ */
+function Elevate(){
+  var aux_value = document.getElementById('display').value;
+
+  aux_value = aux_value * aux_value;
+
+  document.getElementById('display').value = aux_value;
+}
+
+/**
+ * @brief Function to divide 1 with another value.
+ */
+function DivideOne(){
+  var aux_value = document.getElementById('display').value;
+
+  aux_value = 1 / aux_value;
+
+  document.getElementById('display').value = aux_value;
 }
